839fefccf1fda4303d4cbcf953ba602731e6098a
Allan F Ribeiro <allanfribeiro@gmail.com>
2021-11-06 14:54:16 -0300

    Nota Aluno

---
+++ notaAluno/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace notaAluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1, n2, n3, media;
+            string sitacao;
+
+
+            Console.WriteLine("Primeira nota");
+            n1 = byte.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segunda nota");
+            n2 = byte.Parse(Console.ReadLine());
+
+            Console.WriteLine("terceira nota");
+            n3 = byte.Parse(Console.ReadLine());
+
+            media = (n1 + n2 + n3) / 3;
+
+            if (media >= 7)
+            {
+                sitacao = "Aluno aprovado com média {0}";
+            }
+            else if (media >= 4)
+            {
+                sitacao = "Aluno em prova final com média {0}";
+            }
+            else
+            {
+                sitacao = "Aluno reprovado com média {0}";
+            }
+
+            Console.WriteLine(sitacao, media.ToString("0.00"));
+
+
+        }
+    }
+}
